929627a3be4f06de678d8f16f55b5c6a60929001
Olya Yer <oyeremenko@gmail.com>
2018-12-05 11:26:40 +0000

    update dburl

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ var commentRoutes    = require("./routes/comments"),
     indexRoutes      = require("./routes/index")
 
 
-// mongoose.connect("mongodb://localhost:27017/yelp_camp_v9");
-mongoose.connect('mongodb://ol:bastya5@ds125574.mlab.com:25574/yelp_camp');
+mongoose.connect(process.env.DATABASEURL);
+// mongoose.connect('mongodb://ol:bastya5@ds125574.mlab.com:25574/yelp_camp');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
